1418c0e8fb606e2530802e1a4e737ef7fdb85988
Harsh2220 <harshgajjar145@gmail.com>
2021-05-08 20:16:27 +0530

    commit

---
+++ covid/src/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <a href="#home" class="active">Home</a>
+            <a href="#news">News</a>
+            <a href="#contact">Contact</a>
+            <a href="#about">About</a>
+        </div>
+    )
+}
+
+export default Navbar;
